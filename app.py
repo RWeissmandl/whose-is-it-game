d586f9200c21f43e15713bb70ca7b93552c3395f
rachel weissmandl <rochelgw@gmail.com>
2024-12-14 13:43:47 +0000

    debugging to get images to display on play screen

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
         try:
             image = request.files['image']
             category_of_image = request.form['category']
-            filename = f"{int(time.time())}_{image.filename}"
+            filename = f"{int(time.time() *1000) % 10000}_{image.filename}"
             if category_of_image not in categories:
                 return jsonify({"error": "This category doesn't exist"}), 400
             save_path = os.path.join(uploads_folder, category_of_image, filename)
@@ -28,7 +28,16 @@ def home():
 @app.route('/play')
 def play():
     title = "Who do I belong to?"
-    return render_template('play.html', title=title)
+    category_images = {}
+    array_test = [1, 2, 3]
+    for category in categories:
+        file_path = os.path.join(uploads_folder, category)
+        images = os.listdir(file_path) #list all images in the file_path
+        #file_paths = [os.path.join(file_path, image) for image in images]
+        #file_paths = [os.path.join('uploads', category, image) for image in images] #an array with a filepath to each image
+        file_paths = [os.path.join(category, image) for image in images]
+        category_images[category] = file_paths
+    return render_template('play.html', title=title, categories=categories, category_images=category_images, file_path=file_path, images=images, file_paths=file_paths, array_test=array_test)
 
 if __name__ == '__main__':
     app.run(debug=True)
